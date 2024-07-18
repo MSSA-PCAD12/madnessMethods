2c9cc6fa01543b32912ba2e81a79026186dd2af0
Rhett Chase <126417892+rhettchase@users.noreply.github.com>
2024-07-18 14:43:12 -0700

    adds FindInt solution

--- FindOddIntTest/FindOddIntTest.cs
+++ FindOddIntTest/FindOddIntTest.cs
@@ -1,3 +1,5 @@
+using FindOddInt;
+
 namespace FindOddIntTest
 {
     [TestClass]
--- HighestLowestTest/HighestLowestTest.cs
+++ HighestLowestTest/HighestLowestTest.cs
@@ -1,7 +1,7 @@
 namespace HighestLowestTest
 {
     [TestClass]
-    public class UnitTest1
+    public class HighestLowestTest
     {
         [TestMethod]
         public void TestMethod1()
