2d2ab7dfb8727fd62a0abe067a38837068f45c0c
Quinn K <38480462+kajiwara12@users.noreply.github.com>
2024-07-22 13:08:43 -1000

    Revert "Added logic for the tests. All tests are passing"
    This reverts commit 4902c89edbc03f315e7b334c8e1d49dc4eadc575.

--- MadnessMethodsClass/DivisibleByThree.cs
+++ MadnessMethodsClass/DivisibleByThree.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace MadnessMethodsClass
 {
@@ -11,28 +10,32 @@ namespace MadnessMethodsClass
     {
         public static bool DivisibleByThree(string n)
         {
-            if (n == "")
+            // convert given string to int
+            int intToCheck = int.Parse(n);
+
+            // divide int by 3 and convert to double
+            double num = intToCheck / 3;
+            Console.WriteLine("num: " + num); // console log to check logic
+
+            // convert the double to a string, so that we can check if it contains a period
+            // which would signify it is a decimal, therefore not divisible by 3
+            string stringNum = num.ToString();
+            Console.WriteLine("stringNum: " + stringNum); // console log to check logic
+
+            // if the string contains a . we return false because it is not divisible by 3
+            if (stringNum.Contains('.'))
             {
-
-            return false;
+                Console.WriteLine("false"); // console log to check logic
+                return false;
             }
-            int sum = 0;
-
-            // Step 1: Sum the digits
-            foreach (char digit in n)
+            else // if the string does not contain a . we return true since it is a whole number and divisible by 3
             {
-                sum += digit - '0'; // Convert char to int
+                Console.WriteLine("true"); // console log to check logic
+                return true;
             }
 
-            // Step 2: Check divisibility by 3 without using the modulo operator
-            while (sum >= 3)
-            {
-                sum -= 3;
-            }
-
-            return sum == 0;
+            // if the code above is not reached, throw an exception
+            throw new NotImplementedException();
         }
     }
-
 }
-
