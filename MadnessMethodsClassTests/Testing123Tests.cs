e7b0580eb6127ed2294323cb4f9fbb6509133bf4
AJ Paumier <39337835+20apaumier@users.noreply.github.com>
2024-07-22 11:24:08 -0700

    set up branch for aj/quinn

---
+++ MadnessMethodsClassTests/Testing123Tests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MadnessMethodsClassTests
+{
+    internal class Testing123Tests
+    {
+    }
+}
