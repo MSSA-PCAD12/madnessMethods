e8479ee99cd4b2ede26d405e33873209d97979a7
AJ Paumier <39337835+20apaumier@users.noreply.github.com>
2024-07-22 11:24:08 -0700

    set up branch for aj/quinn

---
+++ MadnessMethodsClass/Testing123.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MadnessMethodsClass
+{
+    public static class Testing123
+    {
+        public static string[] Testing123Method(string[] stringList)
+        {
+
+            // blueprint to set up tests
+            return stringList.ToArray();
+        }
+    }
+}
---
+++ MadnessMethodsClassTests1/Testing123Tests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MadnessMethodsClass;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MadnessMethodsClass.Tests
+{
+    [TestClass()]
+    public class Testing123Tests
+    {
+        public void Testing123MethodTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
