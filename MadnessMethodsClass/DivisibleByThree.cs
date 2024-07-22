41b0fcfd69b99c4f8f451c1d1e347068df53c8ef
Rhett Chase <[200~126417892+rhettchase@users.noreply.github.com~>
2024-07-22 15:08:10 -0700

    adds class and tests for Divisible by three

---
+++ MadnessMethodsClass/DivisibleByThree.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MadnessMethodsClass
+{
+    internal class Three
+    {
+        public static bool DivisibleByThree(string n)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
