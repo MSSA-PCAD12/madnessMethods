378b4e829e3f83d476815cd2e4ad5f2161ea3fd6
Jessil Fuhr <16487314+bjess@users.noreply.github.com>
2024-07-22 15:04:42 -0400

    including branch

--- MadnessMethodsClass/RemoveSmallest.cs
+++ MadnessMethodsClass/RemoveSmallest.cs
@@ -8,5 +8,27 @@ namespace MadnessMethodsClass
 {
     internal class RemoveSmallest
     {
+        // Method to remove the smallest number from an array
+    public static int[] RemoveSmallestMethod(int[] numbers)
+        {
+            // If the array is empty, return the array
+            if (numbers.Length == 0)
+            {
+                return numbers;
+            }
+            // Find the smallest number in the array
+            int min = numbers.Min();
+            int[] result = new int[numbers.Length - 1];
+            int index = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] != min)
+                {
+                    result[index] = numbers[i];
+                    index++;
+                }
+            }
+            return result;
+        }
     }
 }
