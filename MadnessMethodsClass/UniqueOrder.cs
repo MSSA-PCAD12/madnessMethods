dd453fc6e942f47484874b1b8d65747eba86c259
Rhett Chase <[200~126417892+rhettchase@users.noreply.github.com~>
2024-07-24 16:38:58 -0700

    adds unique order passing solution

---
+++ MadnessMethodsClass/UniqueOrder.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MadnessMethodsClass
+{
+    internal class Unique
+    {
+        public static IEnumerable<T> UniqueInOrder<T>(IEnumerable<T> iterable)
+        {
+            if (iterable == null)
+                return new List<T>();
+
+            var unique = new List<T>();
+            T previous = default(T);
+            bool isFirst = true;
+
+            foreach (var item in iterable)
+            {
+                if (isFirst || !item.Equals(previous))
+                {
+                    unique.Add(item);
+                    previous = item;
+                    isFirst = false;
+                }
+            }
+
+            return unique;
+        }
+}
