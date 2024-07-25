10b5c10ac50cf9c252d493f8a652753a7877402a
Rhett Chase <126417892+rhettchase@users.noreply.github.com>
2024-07-25 15:44:16 -0700

    adds basic test Replace Alpha

--- MadnessMethodsTest/ReplaceAlphaTests.cs
+++ MadnessMethodsTest/ReplaceAlphaTests.cs
@@ -12,9 +12,25 @@ namespace MadnessMethodsClass.Tests
     public class ReplaceAlphaTests
     {
         [TestMethod()]
-        public void AlphabetPositionTest()
+        public void BasicTests()
         {
-            Assert.Fail();
+            // Act
+            var result = ReplaceAlpha.AlphabetPosition("The sunset sets at twelve o' clock.");
+
+            // Assert
+            Assert.AreEqual("20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11", result);
+
         }
+
+        //[TestMethod]
+        //public void EmptyString()
+        //{
+        //    // Act
+        //    var result = Unique.UniqueInOrder("");
+
+        //    // Assert
+        //    Assert.IsNotNull(result);
+        //    Assert.AreEqual(0, result.Count());
+        //}
     }
 }
