736d2f9b4d9ee278652664e23c5f2d1364ebebb6
Jessil Fuhr <16487314+bjess@users.noreply.github.com>
2024-07-25 18:29:36 -0400

    New files added for ReplaceAlpha kata

---
+++ MadnessMethodsClass/ReplaceAlpha.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MadnessMethodsClass
+{
+    public static class ReplaceAlpha
+    {
+        public static string AlphabetPosition(string text)
+        {
+            return text;
+        }
+    }
+}
