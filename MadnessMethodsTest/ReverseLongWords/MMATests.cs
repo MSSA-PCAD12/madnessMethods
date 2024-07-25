b54cacf5092a1bb34b0fda7b6ad1a887f42c9264
Rhett Chase <[200~126417892+rhettchase@users.noreply.github.com~>
2024-07-25 12:04:13 -0700

    updates reverseLongwords kata solution and updates MMB with logic to reverse only words > 5 chars

---
+++ MadnessMethodsTest/ReverseLongWords/MMATests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MadnessMethodsClass.ReverseLongWords;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MadnessMethodsClass.ReverseLongWords.Tests
+{
+    [TestClass()]
+    public class MMATests
+    {
+        [TestMethod()]
+        public void MMSplitStringTest()
+        {
+            string input = "abc def ghijklmn 1234";
+            IEnumerable<string> expected = input.Split(' ');
+            Assert.IsTrue(expected.SequenceEqual(input.MMSplitString()));
+
+        }
+
+        [TestMethod()]
+        public void MMSplitEmptyTest()
+        {
+            string input = "";
+            IEnumerable<string> expected = input.Split(' ');
+            Assert.AreEqual(0, input.MMSplitString().Count());
+        }
+    }
+}
